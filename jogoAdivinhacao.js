5500594781d007232e484cf555390b446961b3fa
Boryhit <mateusvog@gmail.com>
2026-04-13 19:30:37 -0300

    alterando aleatorio para entre 100 e 200

--- jogoAdivinhacao.js
+++ jogoAdivinhacao.js
@@ -5,7 +5,7 @@ const rl = readline.createInterface({
 });
 
 function getRandom() {
-  return Math.floor((Math.random() * 100) + 1);
+  return Math.floor((Math.random() * 100) + 100);
 }
 
 let aleatory = getRandom();
